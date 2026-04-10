550d3bd2279e0f8987ffb444b21860edfc7d431b
phj77 <parkhj000720@gmail.com>
2026-04-10 18:16:08 +0900

    new files

--- models/model.py
+++ models/model.py
@@ -5,4 +5,12 @@ import torch.nn.functional as F
 class conv2d_block(nn.Module):
     def __init__(self, in_ch, out_ch, kernel, stride=1, padding=0, groups=1):
         super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=kernel, stride=stride, padding=padding, groups=groups),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU()
+        )
         
+    def forward(self, x):
+        x = self.block(x)
+        return x
